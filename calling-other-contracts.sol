47e55c5ac1e6e86fea0a75cbedddd43c5286f21c
ortizfram <ortizfranco48@yahoo.com>
2024-06-18 10:30:16 -0300

    calling other contracts

---
+++ calling-other-contracts.sol
@@ -0,0 +1,34 @@
+// addressSPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract SecretVault {
+    string private secret;
+
+    constructor(string memory _secret){
+        secret = _secret;
+    }
+
+    function setSecret(string memory _secret) external{
+        secret = _secret;
+    }
+
+    function getSecret() external view returns(string memory){
+        return secret;
+    }
+}
+
+contract myContract {
+    SecretVault public secretVault;
+
+    constructor(SecretVault _secretVault){
+        secretVault  = _secretVault;
+    }
+
+    function setSecret(string memory _secret) public {
+        secretVault.setSecret(_secret);
+    }
+
+    function getSecret() public view returns(string memory){
+        return secretVault.getSecret();
+    }
+}
