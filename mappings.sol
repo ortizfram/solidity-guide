01c040186e172d11861670b47ed450465fdbe37f
ortizfram <ortizfranco48@yahoo.com>
2024-06-18 08:31:40 -0300

    mappings functions

--- mappings.sol
+++ mappings.sol
@@ -7,5 +7,19 @@ contract myContract {
     mapping(address=>uint) public balances;
     mapping(address=>bool) public hasVoted;
     mapping(address=>mapping(uint=>bool)) public myMapping;//nested
+// ====================
 
+mapping(uint=>string) public myMapping1;
+
+function get(uint _id) public view returns(string memory){
+    return myMapping1[_id];
+}
+
+function set(uint _id, string memory _value) public {
+    myMapping1[_id]= _value;
+}
+
+function remove(uint _id) public {
+    delete myMapping1[_id];
+}
 }
